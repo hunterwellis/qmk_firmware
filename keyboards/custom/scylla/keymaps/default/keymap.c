7cad1cf0691f8d18595e72520fa5afc766b9406e
Hunter Ellis <elliswhunter@gmail.com>
2025-09-09 23:01:39 -0400

    updated keymap for thumb cluster wiring

--- keyboards/custom/scylla/keymaps/default/keymap.c
+++ keyboards/custom/scylla/keymaps/default/keymap.c
@@ -36,8 +36,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
    KC_DEL , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
    KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_LSFT,
-                                                LOWER  , KC_ENTER, KC_DEL , KC_BSPC , KC_SPACE, RAISE,
-                                                         KC_LALT, KC_LCTL , KC_RCTL ,  KC_RALT
+                                                KC_ENTER , KC_LCTL , LOWER , RAISE , KC_RCTL , KC_SPC,
+                                                         KC_DEL , KC_LALT , KC_RALT , KC_BSPC
     ),
 
    [_RAISE] = LAYOUT(
@@ -94,8 +94,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                          _______, _______, _______, _______
    ),
 
-   [_ADJUST] = LAYOUT(
-   /* ADJUST
+   [_SYSTEM] = LAYOUT(
+   /* SYSTEM
    * .-----------------------------------------.                                  .-----------------------------------------.
    * | BASE |      |      |      |      | BASE |                                  | BASE |      |      |      |      | BASE |
    * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
--- keyboards/custom/scylla/promicro/config.h
+++ keyboards/custom/scylla/promicro/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define MASTER_RIGHT
-// #define MASTER_LEFT
+// #define MASTER_RIGHT
+#define MASTER_LEFT
 
 #define USE_SERIAL
 
